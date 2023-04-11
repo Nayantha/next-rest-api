c7cd9352ee0cae9e9f8d3c1a0f08d3d61de64bf4
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-11 17:51:52 +0530

    import Inter and classNameMerger.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
 import '@/styles/globals.css'
 import React from "react";
+import {Inter} from "next/font/google";
+import {classNameMerge} from "@/lib/utils";
 
+const inter = Inter({subsets: ["latin"]})
 export default function RootLayout({
                                        children,
                                    }: {
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
+        <html lang="en" className={classNameMerge()}>
         <body>{children}</body>
         </html>
     )
