75c10b347b7e7da7f734ed3335167c93dd9d2c5a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-11 20:10:23 +0530

    import dropdown menu.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,12 +1,13 @@
 "use client"
 import {FC} from 'react'
 import {useTheme} from 'next-themes'
+import {DropdownMenu} from "@/ui/DropdownMenu"
 
 interface ThemeToggleProps {
 }
 
 const ThemeToggle: FC<ThemeToggleProps> = ({}) => {
     const {setTheme} = useTheme()
-    return <div>ThemeToggle</div>
+    return <DropdownMenu></DropdownMenu>
 }
 export default ThemeToggle
