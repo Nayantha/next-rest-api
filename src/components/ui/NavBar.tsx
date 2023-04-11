b7ed0735f90bb51ead3a65e2d7ba1a080a6724cc
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-11 18:33:04 +0530

    complete the skeleton of the nav bar.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

---
+++ src/components/ui/NavBar.tsx
@@ -0,0 +1,35 @@
+import {getServerSession} from "next-auth";
+import Link from "next/link";
+
+interface NavBarProps {
+}
+
+const NavBar = async ({}) => {
+    const session = await getServerSession()
+    return <nav
+        className={"fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between"}>
+        <div className={"container max-w-7xl mx-auto w-full flex justify-between items-center"}>
+            <Link href={"/"} className={buttonVariants({variants: "link"})}>
+                API With NEXT.JS 13 1.0
+            </Link>
+            <div className={"md-hidden"}>
+                <ThemeToggle/>
+            </div>
+            <div className={"hidded md:flex gap-4"}>
+                <ThemeToggle/>
+                <Link href={"/documentation"} className={buttonVariants({variants: "ghost"})}>
+                    Documentation
+                </Link>
+                {session ?
+                    <>
+                        <Link className={buttonVariants({variants: "ghost"})} href={"/dashboard"}>
+                            Dashboard
+                        </Link>
+                        <SignOutButton/>
+                    </> :
+                    <SignInButton/>}
+            </div>
+        </div>
+    </nav>
+}
+export default NavBar
