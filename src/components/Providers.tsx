f59266900ca064ae4e5ef6a8246f15503eee7a17
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-11 18:10:28 +0530

    complete the provider component.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,9 +1,14 @@
-import {FC} from 'react'
+"use client"
+import {ReactNode} from 'react'
+import {ThemeProvider} from "next-themes";
+import {SessionProvider} from "next-auth/react";
 
-interface ProvidersProps {
-}
 
-const Providers: FC<ProvidersProps> = ({}) => {
-    return <div>Providers</div>
+const Providers = ({children}: { children: ReactNode }) => {
+    return <ThemeProvider attribute={"class"} defaultTheme={"system"} enableSystem>
+        <SessionProvider>
+            {children}
+        </SessionProvider>
+    </ThemeProvider>
 }
 export default Providers
