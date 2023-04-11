eefc1fc54f0a3140c1d11a16a2315b989cea93e1
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-11 20:06:20 +0530

    made client component and import useTheme hook.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,9 +1,12 @@
+"use client"
 import {FC} from 'react'
+import {useTheme} from 'next-themes'
 
 interface ThemeToggleProps {
 }
 
 const ThemeToggle: FC<ThemeToggleProps> = ({}) => {
+    const {setTheme} = useTheme()
     return <div>ThemeToggle</div>
 }
 export default ThemeToggle
