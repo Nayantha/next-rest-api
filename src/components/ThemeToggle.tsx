d7ee97a45bde86cb1a79759ad6d3ac56dd2da477
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-11 21:10:39 +0530

    add drop down menu items.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,9 +1,9 @@
 "use client"
 import {FC} from 'react'
 import {useTheme} from 'next-themes'
-import {DropdownMenu, DropdownMenuTrigger} from "@/ui/DropdownMenu"
+import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/ui/DropdownMenu"
 import Button from "@/ui/Button";
-import {Moon, Sun} from "lucide-react";
+import {Laptop, Moon, Sun} from "lucide-react";
 
 interface ThemeToggleProps {
 }
@@ -20,6 +20,26 @@ const ThemeToggle: FC<ThemeToggleProps> = ({}) => {
                 <span className={"sr-only"}>Toggle Theme</span>
             </Button>
         </DropdownMenuTrigger>
+        <DropdownMenuContent align={"end"} forceMount>
+            <DropdownMenuItem onClick={() => {
+                setTheme("light")
+            }}>
+                <Sun className={"mr-2 h-4 w-4"}/>
+                <span>Light</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => {
+                setTheme("light")
+            }}>
+                <Moon className={"mr-2 h-4 w-4"}/>
+                <span>Dark</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => {
+                setTheme("system")
+            }}>
+                <Laptop className={"mr-2 h-4 w-4"}/>
+                <span>System</span>
+            </DropdownMenuItem>
+        </DropdownMenuContent>
     </DropdownMenu>
 }
 export default ThemeToggle
