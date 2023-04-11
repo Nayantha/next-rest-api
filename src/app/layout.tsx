1de3df983235183bd03c8138ae46b3d40c680f2d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-11 18:00:49 +0530

    add classes for html and body tags.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,22 @@ import '@/styles/globals.css'
 import React from "react";
 import {Inter} from "next/font/google";
 import {classNameMerge} from "@/lib/utils";
+import Providers from "@/components/Providers";
 
 const inter = Inter({subsets: ["latin"]})
-export default function RootLayout({
-                                       children,
-                                   }: {
+
+export default function RootLayout({children,}: {
     children: React.ReactNode
 }) {
     return (
-        <html lang="en" className={classNameMerge()}>
-        <body>{children}</body>
+        <html lang="en" className={classNameMerge(
+            "bg-white text-slate-900 antialiased", inter.className
+        )}>
+        <body className={"min-h-screen bg-slate-50 dark:bg-slate-900 antialiased"}>
+        <Providers>
+            {children}
+        </Providers>
+        </body>
         </html>
     )
 }
