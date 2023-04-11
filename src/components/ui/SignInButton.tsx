1d3f4be27000c72c5ea4bd597572d3ee55bd5128
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-11 19:43:13 +0530

    add sign in logic and on click event.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- src/components/ui/SignInButton.tsx
+++ src/components/ui/SignInButton.tsx
@@ -1,12 +1,30 @@
 "use client"
 import {FC, useState} from 'react'
 import Button from "@/ui/Button";
+import {signIn} from "next-auth/react";
+import toast from "react-hot-toast";
 
 interface SignInButtonProps {
 }
 
 const SignInButton: FC<SignInButtonProps> = ({}) => {
     const [isLoading, setLoading] = useState<boolean>(false)
-    return <Button></Button>
+
+    const signInWithGoogle = async () => {
+        setLoading(true)
+        try {
+            await signIn("Google")
+        } catch (error) {
+            toast({
+                key: undefined,
+                props: undefined,
+                type: "error",
+                title: "Error Signing in",
+                message: "Please try again later."
+            })
+        }
+    }
+
+    return <Button onClick={signInWithGoogle} isLoading={isLoading}>Sign in</Button>
 }
 export default SignInButton
